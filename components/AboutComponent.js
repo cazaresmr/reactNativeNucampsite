3e897b5834ad29afb8a98957d142524505abd797
cazaresmr <michaelcazares@gmail.com>
2021-09-02 22:31:20 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,13 +3,13 @@ import { ScrollView, Text, FlatList } from "react-native"
 import { Card, ListItem } from "react-native-elements"
 import { connect } from "react-redux"
 import { baseUrl } from "../shared/baseUrl"
+import Loading from "./LoadingComponent"
 
 const mapStateToProps = (state) => {
 	return {
 		partners: state.partners,
 	}
 }
-
 
 function Mission() {
 	return (
@@ -26,7 +26,6 @@ function Mission() {
 }
 
 class About extends Component {
-
 	static navigationOptions = {
 		title: "About Us",
 	}
@@ -42,21 +41,39 @@ class About extends Component {
 			)
 		}
 
+		if (this.props.partners.isLoading) {
+			return (
+				<ScrollView>
+					<Mission />
+					<Card title='Community Partners'>
+						<Loading />
+					</Card>
+				</ScrollView>
+			)
+		}
+		if (this.props.partners.errMess) {
+			return (
+				<ScrollView>
+					<Mission />
+					<Card title='Community Partners'>
+						<Text>{this.props.partners.errMess}</Text>
+					</Card>
+				</ScrollView>
+			)
+		}
 		return (
 			<ScrollView>
-				<Mission />
-				<Card title='Community Partners'>
-                    <FlatList 
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
-                    />
-				</Card>
+					<Mission />
+					<Card title='Community Partners'>
+						<FlatList
+							data={this.props.partners.partners}
+							renderItem={renderPartner}
+							keyExtractor={(item) => item.id.toString()}
+						/>
+					</Card>
 			</ScrollView>
 		)
 	}
 }
 
-export default connect(mapStateToProps)(About);
-
-
+export default connect(mapStateToProps)(About)
